1960b32811161e69bf6c09aea9d15b297ae37859
mfnintd <2nghin0tram16@gmail.com>
2024-06-24 14:08:38 +0700

    add theme

--- lib/app/providers/theme_provider.dart
+++ lib/app/providers/theme_provider.dart
@@ -2,4 +2,9 @@ import 'package:flutter/material.dart';
 
 class ThemeProvider extends ChangeNotifier {
   //final is
+  ThemeData themeData = ThemeData.light();
+  void setThemeData(ThemeData themeData) {
+    this.themeData = themeData;
+    notifyListeners();
+  }
 }
